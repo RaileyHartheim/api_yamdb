63235b6f9993bab05a19b618e9f41d89ca14efb7
Александр Мамонов <Alex.A.Mamonov@yandex.ru>
2022-05-09 21:16:47 +0300

    create reviews model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from django.core.validators import MaxValueValidator
+
+User = get_user_model()
+
+class reviews(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,  verbose_name='Автор'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True
+        )
+    text = models.TextField(
+        max_length=255, verbose_name='Отзыв'
+        )
+    score = models.IntegerField(
+        validators=[MaxValueValidator(10)], verbose_name='Оценка'
+    )
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE
+    )
+
+class comments(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comment_author', verbose_name='Автор')
+    rewiew = models.ForeignKey(
+        reviews, on_delete=models.CASCADE, related_name='comment', blank=True)
+    text = models.TextField(max_length=255, verbose_name='Коментарий')
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
