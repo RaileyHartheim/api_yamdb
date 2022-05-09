834c58771015d1ba6f41719797a3509faf18d2ed
Yulia Kazhaeva <melancholia.raiven@gmail.com>
2022-05-09 11:20:34 +0500

    Add custom User model, update settings.py, requirements.txt

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -16,6 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'api',
     'reviews',
     'users'
@@ -77,7 +80,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -88,7 +91,22 @@ USE_L10N = True
 USE_TZ = True
 
 
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,54 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+
+USER_ROLES = [
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор')
+]
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name='E-mail',
+        unique=True,
+        max_length=254,
+        blank=False
+    )
+    bio = models.TextField(
+        verbose_name='Биография',
+        max_length=500,
+        blank=True
+    )
+    role = models.TextField(
+        verbose_name='Роль',
+        choices=USER_ROLES,
+        default='user'
+    )
+    confirmation_code = models.CharField(
+        verbose_name='Код подтверждения',
+        max_length=50,
+        blank=True
+    )
+
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_admin(self):
+        return self.is_superuser or self.role == 'admin'
+
+    @property
+    def is_moderator(self):
+        return self.role == 'moderator'
+
+    @property
+    def is_user(self):
+        return self.role == 'user'
