f875ba2db015e77ecc3472fbd9980bda6a08edb5
Yulia Kazhaeva <melancholia.raiven@gmail.com>
2022-05-09 20:22:29 +0500

    Add API custom permissions, urls for API in project urls.py

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,29 @@
+from rest_framework import permissions
+
+
+class AdminPermission(permissions.BasePermission):
+    """for managing users"""
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_admin
+
+
+class AdminOrReadOnlyPermission(permissions.BasePermission):
+    """for categories, genres and titles"""
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_authenticated
+                    and request.user.is_admin))
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_authenticated and request.user.is_admin))
+
+
+class AuthorOrModerOrAdminPermission(permissions.BasePermission):
+    """for edit/delete reviews and comments"""
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_authenticated
+                    and (obj.owner == request.user
+                         or request.user.is_admin
+                         or request.user.is_moderator)))
