49d4ec867ee5ff61e04542c668f6ed03321ff6ba
Кирилл Щипило <darksfs@yandex.ru>
2022-05-10 16:21:49 +0300

    fixing urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,12 +3,12 @@ from rest_framework.routers import DefaultRouter
 
 from .views import CategoryViewSet, GenreViewSet, TitleViewSet
 
-router = DefaultRouter()
+router_v1 = DefaultRouter()
 
-router.register(r'titles', TitleViewSet, basename='titles')
-router.register(r'genres', GenreViewSet, basename='genres')
-router.register(r'categories', CategoryViewSet, basename='categories')
+router_v1.register(r'titles', TitleViewSet, basename='titles')
+router_v1.register(r'genres', GenreViewSet, basename='genres')
+router_v1.register(r'categories', CategoryViewSet, basename='categories')
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
