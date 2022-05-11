77506236010838d801d0dbdca58c85d16607bed8
Yulia Kazhaeva <melancholia.raiven@gmail.com>
2022-05-11 19:48:32 +0500

    Add registration and authentication part in api app

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,115 @@
+from django.db.models import Avg
+from django.utils import timezone
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from reviews.models import Category, Genre, Title
+from users.models import User
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class TitleCreateSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Category.objects.all(),
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Genre.objects.all(), many=True
+    )
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'description', 'genre', 'category'
+        )
+
+    def validate_year(self, value):
+        current_year = timezone.now().year
+        if not 0 <= value <= current_year:
+            raise serializers.ValidationError(
+                'Укажите год создания произведения.'
+            )
+        return value
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    rating = serializers.SerializerMethodField()
+    category = CategorySerializer()
+    genre = GenreSerializer(many=True)
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
+
+    def get_rating(self, obj):
+        rating = obj.reviews.aggregate(Avg('score')).get('score__avg')
+        if not rating:
+            return rating
+        return round(rating, 1)
+
+
+class AdminUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'username', 'first_name', 'last_name', 'email', 'role', 'bio'
+        )
+
+    def validate_username(self, value):
+        """Checks if user is trying to use 'me' as username"""
+        if value == 'me':
+            raise serializers.ValidationError("Недопустимое имя пользователя")
+        return value
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username', 'first_name', 'last_name', 'email', 'role', 'bio'
+        )
+        read_only_fields = ('role',)
+
+    def validate_username(self, value):
+        """Checks if user is trying to use 'me' as username"""
+        if value == 'me':
+            raise serializers.ValidationError("Недопустимое имя пользователя")
+        return value
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email',)
+
+    def validate_username(self, value):
+        """Checks if user is trying to use 'me' as username"""
+        if value == 'me':
+            raise serializers.ValidationError("Недопустимое имя пользователя")
+        return value
+
+
+class TokenSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    confirmation_code = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'confirmation_code')
---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import (CategoryViewSet, GenreViewSet, SignUpViewSet,
+                    TokenViewSet, TitleViewSet, UserViewSet)
+
+router_v1 = DefaultRouter()
+
+router_v1.register(r'titles', TitleViewSet, basename='titles')
+router_v1.register(r'genres', GenreViewSet, basename='genres')
+router_v1.register(r'categories', CategoryViewSet, basename='categories')
+router_v1.register(r'users', UserViewSet, basename='users')
+router_v1.register('auth/signup', SignUpViewSet, basename='signup')
+router_v1.register('auth/token', TokenViewSet, basename='token')
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+]
