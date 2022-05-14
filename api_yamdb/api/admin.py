52048d1f4d8ed6ed30d9422d326a206771e9d964
Yulia Kazhaeva <41512321+RaileyHartheim@users.noreply.github.com>
2022-05-14 14:44:04 +0500

    Fix/misc (#27)
    * Fix some moments
    
    * Fix becaue of PEP8

--- api_yamdb/api/admin.py
+++ api_yamdb/api/admin.py
@@ -9,11 +9,13 @@ class TitleAdmin(admin.ModelAdmin):
     list_filter = ('year',)
     empty_value_display = '-пусто-'
 
+
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug',)
     search_fields = ('name',)
     empty_value_display = '-пусто-'
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
